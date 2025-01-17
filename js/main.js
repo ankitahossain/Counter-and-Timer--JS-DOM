6aea5201ead78601b76975f4c05dca57b7a9db9b
ankitahossain <ankitahossaina@gmail.com>
2025-01-17 10:54:20 +0600

    project code

---
+++ js/main.js
@@ -0,0 +1,101 @@
+//get all html element
+
+// const counter = document.getElementById("counter");
+// const btn = document.querySelectorAll(".btn");
+// let count=0;
+// btn.forEach((btn) => {
+//     btn.addEventListener('click', (eachBtn) => {
+//         let className = eachBtn.target.className;
+//         let classArr = className.split(" ");
+//         let exactClassName = classArr[classArr.length - 1];
+
+
+
+        
+        // if(exactClassName == "increase"){
+        //     count++;
+        // }
+        // else if(exactClassName == "decrease"){
+        //     count --;
+        // }
+        // else{
+        //     count= 0;
+           
+        // }
+
+    
+
+        // counter.innerHTML =count;
+        
+
+//         if(className.includes("increase")){
+//             count ++;
+//         }
+
+//         else if(className.includes("decrease")){
+//             count --;
+//         }
+//         else{
+//             count = 0;
+//         }
+
+//         counter.innerHTML =count;
+
+
+
+
+//     })
+
+// });
+
+
+
+
+//Timer which run for 30 sec than stop autometically
+
+// let count =0;
+
+// let interval=setInterval(()=>{
+//     counter.innerHTML =count++;
+
+//     if(count>30){
+//         clearInterval(interval);
+//     }
+
+// },500)
+
+
+
+//get all html element
+// const counter=document.getElementById("counter");
+// const btn =document.querySelectorAll(".btn");
+// let count=0;
+// btn.forEach((btn)=>{
+//     btn.addEventListener('click',(eachBtn)=>{
+//      let classname=eachBtn.target.className;
+//       if(classname.includes("increase")){
+//         count++;
+//       }
+//       else if(classname.includes("decrease")){
+//         count--;
+//       }
+//       else{
+//         count=0;
+//       }
+
+//      counter.innerHTML=count;
+
+//     });
+// });
+
+//set timer for 30 sec interval
+let count=0;
+
+let interval=setInterval(()=>{
+    counter.innerHTML=count++;
+    if(count>30){
+        clearInterval(interval);
+    }
+},1000);
+
+
